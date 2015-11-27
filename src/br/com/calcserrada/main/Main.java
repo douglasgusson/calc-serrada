9703dfa16a8eff8f4d0f56292f66c144697b58e7
Douglas Gusson <douglasgusson@gmail.com>
2015-11-27 15:34:14 -0200

    Mundança de look

--- src/br/com/calcserrada/main/Main.java
+++ src/br/com/calcserrada/main/Main.java
@@ -1,12 +1,6 @@
 
 package br.com.calcserrada.main;
 
-import de.javasoft.plaf.synthetica.SyntheticaAluOxideLookAndFeel;
-import java.text.ParseException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 import br.com.calcserrada.views.FrmPrincipal;
 
 /**
@@ -17,11 +11,6 @@ public class Main {
 
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(() -> {
-            try {
-                UIManager.setLookAndFeel(new SyntheticaAluOxideLookAndFeel());
-            } catch (UnsupportedLookAndFeelException | ParseException ex) {
-                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-            }
             new FrmPrincipal().setVisible(true);
         });
 
